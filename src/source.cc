0e70045d882e409161f6b6091a8539a5de30e5c6
eidheim <eidheim@gmail.com>
2018-07-18 22:56:37 +0200

    Minor addition to Source::View::on_key_press_event_smart_inserts

--- src/source.cc
+++ src/source.cc
@@ -2533,7 +2533,7 @@ bool Source::View::on_key_press_event_smart_inserts(GdkEventKey *key) {
 
   auto allow_insertion = [](const Gtk::TextIter &iter) {
     if(iter.ends_line() || *iter == ' ' || *iter == '\t' || *iter == ';' || *iter == ',' ||
-       *iter == ')' || *iter == '[' || *iter == ']' || *iter == '{' || *iter == '}' || *iter == '<' || *iter == '>')
+       *iter == ')' || *iter == '[' || *iter == ']' || *iter == '{' || *iter == '}' || *iter == '<' || *iter == '>' || *iter == '/')
       return true;
     return false;
   };
