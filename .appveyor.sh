2f52f4703d693cabdb79b1108cf7c802c92ce752
Jørgen Lien Sellæg <j.sellaeg@sportradar.com>
2018-09-28 12:43:51 +0200

    get debug information from bash

--- .appveyor.sh
+++ .appveyor.sh
@@ -1,7 +1,8 @@
+set -x
 cd ${APPVEYOR_BUILD_FOLDER}
 git submodule update --init --recursive
 mkdir -p build
 cd build
 export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/c/WINDOWS/system32:/c/WINDOWS:/c/WINDOWS/System32/Wbem:/c/WINDOWS/System32/WindowsPowerShell/v1.0:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
-exec sh -c "cmake -G\"MSYS Makefiles\" -DCMAKE_INSTALL_PREFIX=/mingw64 -DBUILD_TESTING=1 .. && make && make test"
+exec bash -c "set -x && cmake -G\"MSYS Makefiles\" -DCMAKE_INSTALL_PREFIX=/mingw64 -DBUILD_TESTING=1 .. && make && make test"
 
